b6864e29d3fa854e4927e541d0820379cc94995e
zhengkun1 <398786764@qq.com>
2017-12-23 13:23:11 +0800

    去掉页面跳转按钮的背景色

--- MyBlog/src/layout/Basiclayout.js
+++ MyBlog/src/layout/Basiclayout.js
@@ -3,7 +3,7 @@ import { Layout, Menu, Row, Col } from 'antd';
 // import default from 'antd/lib/card/Grid';
 import { Link, Route, Switch } from 'dva/router';
 import styles from '../layout/Basiclayout.less';
-// import A from '../routes/homepage';
+import A from '../routes/homepage';
 import B from '../routes/self';
 import Write from '../routes/write';
 import about from '../routes/about';
@@ -30,6 +30,14 @@ class Basiclayout extends React.PureComponent {
     const layout = (
       <div className={styles.div}>
         <Layout>
+          <Content style={{ minHeight: 'calc(100vh - 100px)' }} className={styles.content}>
+            <Switch>
+              <Route path="/home" component={A} />
+              <Route path="/about" component={about} />
+              <Route path="/write" component={Write} />
+              <Route path="/" component={B} />
+            </Switch>
+          </Content>
           <Header
             className={styles.header}
           >
@@ -39,8 +47,10 @@ class Basiclayout extends React.PureComponent {
                 <Menu
                   onClick={this.handleClick}
                   selectedKeys={[this.state.current]}
+                  mode="horizontal"
                   className={styles.menu}
                 >
+                  <Menu.Item key="0"><Link to="home" style={{ fontSize: '30px' }}>HOME</Link></Menu.Item>
                   <Menu.Item key="1"><Link to="/" style={{ fontSize: '30px' }}>SELF</Link></Menu.Item>
                   <Menu.Item key="2"><Link to="/write" style={{ fontSize: '30px' }}>ESSAY</Link></Menu.Item>
                   <Menu.Item key="3"><Link to="about" style={{ fontSize: '30px' }}>ABOUT</Link></Menu.Item>
@@ -48,19 +58,12 @@ class Basiclayout extends React.PureComponent {
               </Col>
             </Row>
           </Header>
-          <Content style={{ minHeight: 'calc(100vh - 100px)' }} className={styles.content}>
-            <Switch>
-              <Route path="/about" component={about} />
-              <Route path="/write" component={Write} />
-              <Route path="/" component={B} />
-            </Switch>
-          </Content>
           <Footer className={styles.footer}>
             <div>
               <p>@2017-12-14 Made By 10K</p>
-              <Link to="/self" style={{ marginRight: '10px' }}>SELF</Link>
-              <Link to="/write" style={{ marginRight: '10px' }}>ESSAY</Link>
-              <Link to="/about">ABOUT</Link>
+              <Link to="/self" style={{ marginRight: '10px', color: 'black' }}>SELF</Link>
+              <Link to="/write" style={{ marginRight: '10px', color: 'black' }}>ESSAY</Link>
+              <Link to="/about" style={{ color: 'black' }}>ABOUT</Link>
             </div>
           </Footer>
         </Layout>
