da002fedec64b9acce97008bf1b998b21833d327
zhengkun1 <398786764@qq.com>
2017-12-29 15:47:38 +0800

    model

---
+++ MyBlog/src/models/global.js
@@ -0,0 +1,24 @@
+export default {
+  namespace: 'global',
+
+  state: {
+    essay: '',
+  },
+
+  effects: {
+    *self(_, { put }) {
+      yield put({
+        type: 'add',
+        payload: 'nihao',
+      });
+    },
+  },
+  reducers: {
+    add(state, payload) {
+      return {
+        ...state,
+        essay: payload,
+      };
+    },
+  },
+};
--- MyBlog/src/routes/contact.js
+++ MyBlog/src/routes/contact.js
@@ -1,13 +1,18 @@
 import React, { PureComponent } from 'react';
 // import { Icon } from 'antd';
+import { connect } from 'dva';
 import styles from './contact.less';
 // import default from 'antd/lib/input';
-
-export default class About extends PureComponent {
+// @connect(state => ({
+//   aaa: state.global.essay,
+// }))
+class Contact extends PureComponent {
   render() {
+    const { aaa } = this.props;
     const layout = (
       <div className={styles.div}>
         <img className={styles.img} alt="图片无法加载" src="http://isujin.com/wp-content/uploads/2017/11/wallhaven-37578.jpg" />
+        <div>{aaa}</div>
       </div>
     );
     return (
@@ -15,3 +20,6 @@ export default class About extends PureComponent {
     );
   }
 }
+export default connect(state => ({
+  aaa: state.global.essay,
+}))(Contact);
