4d7e218e4dd4b4ea95cf57a77f7e02c7c3109b27
zhengkun1 <398786764@qq.com>
2017-12-29 16:55:42 +0800

    train

--- MyBlog/src/routes/contact.js
+++ MyBlog/src/routes/contact.js
@@ -2,17 +2,23 @@ import React, { PureComponent } from 'react';
 // import { Icon } from 'antd';
 import { connect } from 'dva';
 import styles from './contact.less';
-// import default from 'antd/lib/input';
-// @connect(state => ({
-//   aaa: state.global.essay,
-// }))
+
+
 class Contact extends PureComponent {
+  componentDidMount() {
+    this.props.dispatch({
+      type: 'global/self',
+    });
+  }
   render() {
     const { aaa } = this.props;
+    console.log(aaa.payload);
     const layout = (
-      <div className={styles.div}>
-        <img className={styles.img} alt="图片无法加载" src="http://isujin.com/wp-content/uploads/2017/11/wallhaven-37578.jpg" />
-        <div>{aaa}</div>
+      <div>
+        <div className={styles.div}>
+          <img className={styles.img} alt="图片无法加载" src="http://isujin.com/wp-content/uploads/2017/11/wallhaven-37578.jpg" />
+        </div>
+        <div>{aaa.payload}</div>
       </div>
     );
     return (
